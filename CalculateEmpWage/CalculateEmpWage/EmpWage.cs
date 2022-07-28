83efb220c72bde571ea25ccd4aab4ee4c2e8b769
mohan <sahumohan338@gmail.com>
2022-07-27 23:11:17 -0700

    [mohan]add: Calculate Part TIme Employee Wage

--- CalculateEmpWage/CalculateEmpWage/EmpWage.cs
+++ CalculateEmpWage/CalculateEmpWage/EmpWage.cs
@@ -11,19 +11,25 @@ namespace CalculateEmpWage
         public static void CheckEmployee()
         {
             int Is_FullTime = 1;
+            int Is_PartTime = 2;
             int Emp_Rate_Per_Hrs = 20;
-
+            
             int EmpHrs = 0;
             int EmpWage = 0;
 
             Random random = new Random();
-            int Check = random.Next(2);
+            int Check = random.Next(3);
 
             if(Check==Is_FullTime)
             {
                 Console.WriteLine("Employee Is Present");
                 EmpHrs = 8;
 
+            }
+            else if(Check==Is_PartTime)
+            {
+                Console.WriteLine("Employee Is Parttime Present");
+                EmpHrs = 4;
             }
             else
             {
