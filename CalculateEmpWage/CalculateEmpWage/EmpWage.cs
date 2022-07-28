2f8f88cab04b3c7e67b299569918c65010b02de4
mohan <sahumohan338@gmail.com>
2022-07-27 23:35:46 -0700

    [mohan]add: Calculate EmpWage A Month

--- CalculateEmpWage/CalculateEmpWage/EmpWage.cs
+++ CalculateEmpWage/CalculateEmpWage/EmpWage.cs
@@ -13,30 +13,36 @@ namespace CalculateEmpWage
             int Is_FullTime = 1;
             int Is_PartTime = 2;
             int Emp_Rate_Per_Hrs = 20;
+            int Emp_Wrk_Day_Per_Month = 20;
             
             int EmpHrs = 0;
             int EmpWage = 0;
 
-            Random random = new Random();
-            int Check = random.Next(3);
 
-            switch(Check)
+            for (int i = 1; i <= Emp_Wrk_Day_Per_Month; i++)
             {
-                case 0:
-                Console.WriteLine("Employee Is Present");
-                EmpHrs = 8;
-                break;
-                case 1:
-                Console.WriteLine("Employee Is Parttime Present");
-                EmpHrs = 4;
-                break;
-                case 2:
-                Console.WriteLine("Employee Is Absent");
-                EmpHrs = 0;
-                    break;
+                Random random = new Random();
+                int Check = random.Next(3);
 
+
+                switch (Check)
+                {
+                    case 0:
+                        Console.WriteLine("Day: "+i+" Employee Is Present");
+                        EmpHrs += 8;
+                        break;
+                    case 1:
+                        Console.WriteLine("Day: "+i +" Employee Is Parttime Present");
+                        EmpHrs += 4;
+                        break;
+                    case 2:
+                        Console.WriteLine("Day: "+i +" Employee Is Absent");
+                        EmpHrs = 0;
+                        break;
+
+
+                }
             }
-         
            
             EmpWage = EmpHrs * Emp_Rate_Per_Hrs;
             Console.WriteLine("Total Empwage: " + EmpWage);
