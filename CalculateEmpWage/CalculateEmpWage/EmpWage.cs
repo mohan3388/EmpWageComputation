be54f95284ed00ac755388ce462c6fde798071de
mohan <sahumohan338@gmail.com>
2022-07-27 22:53:07 -0700

    [mohan]add: Employee Wage Computation

---
+++ CalculateEmpWage/CalculateEmpWage/EmpWage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateEmpWage
+{
+    public class EmpWage
+    {
+        public static void CheckEmployee()
+        {
+            int Is_FullTime = 1;
+
+            Random random = new Random();
+            int Check = random.Next(2);
+
+            if(Check==Is_FullTime)
+            {
+                Console.WriteLine("Employee Is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee Is Absent");
+            }
+        }
+    }
+}
---
+++ CalculateEmpWage/CalculateEmpWage/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CalculateEmpWage
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            EmpWage.CheckEmployee();
+        }
+    }
+}
